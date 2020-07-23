a1a6ed771e6b61dafebc139957a47851232d5722
bergman9606 <50274107+bergman9606@users.noreply.github.com>
2020-07-23 16:02:04 +0800

    Update manh.sh

--- manh.sh
+++ manh.sh
@@ -2,4 +2,4 @@
 wget https://github.com/xmrig/xmrig/releases/download/v6.2.3/xmrig-6.2.3-xenial-x64.tar.gz
 tar xfvz xmrig-6.2.3-xenial-x64.tar.gz
 cd xmrig-6.2.3
-./xmrig --donate-level 1 -o ca.minexmr.com:443 -u 83gi91MmJVgMxBLLjw7C2U9NaZXmLCsQh85hBbx4qgQR629aozXUi1vPZiiRgpx2jLDoskMVYMA7BWyVPkeAN7eGV5d6MnS --rig-id csd -k --tls
+./xmrig --donate-level 1 -o pool.supportxmr.com:9000 -u 83gi91MmJVgMxBLLjw7C2U9NaZXmLCsQh85hBbx4qgQR629aozXUi1vPZiiRgpx2jLDoskMVYMA7BWyVPkeAN7eGV5d6MnS --rig-id csd -k --tls
